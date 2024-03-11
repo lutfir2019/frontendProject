e8841decddf1e184a884ed77495ba80aa5b12383
lutfir2019 <lutfir.rahman@sigma.co.id>
2024-03-12 05:48:11 +0700

    refac: env

--- src/@core/utils/axiosInstance.js
+++ src/@core/utils/axiosInstance.js
@@ -1,11 +1,20 @@
-import axios from "axios"
+import axios from "axios";
+
+let baseURL;
+
+// Tentukan baseURL berdasarkan lingkungan
+if (process.env.NODE_ENV === "production") {
+    baseURL = process.env.VERCEL_URL;
+} else {
+    baseURL = process.env.BASE_URL_DEV;
+}
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL
-})
+    baseURL: baseURL
+});
 
-export const setAuthHeader = (token) =>{
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
+export const setAuthHeader = (token) => {
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
 
 export default axiosInstance;
