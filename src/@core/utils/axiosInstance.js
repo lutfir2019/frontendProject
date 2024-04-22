906c9827da878bfbc068f7f69d6749ab4291c0d0
lutfir2019 <lutfirrahman2019@gmail.com>
2024-04-22 23:57:06 +0700

    refactor next url

--- src/@core/utils/axiosInstance.js
+++ src/@core/utils/axiosInstance.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const getBaseURL = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.VERCEL_URL
-  } else {
-    return process.env.NEXT_PUBLIC_BASE_URL
-  }
+  return process.env.NEXT_PUBLIC_BASE_URL
 }
 
 const axiosInstance = axios.create({
