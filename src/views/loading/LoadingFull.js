72d154a474bfccfb0d6677a505a7d2728f1fd8c0
lutfir2019 <lutfirrahman2019@gmail.com>
2024-04-25 23:49:08 +0700

    fix: modal loading

--- src/views/loading/LoadingFull.js
+++ src/views/loading/LoadingFull.js
@@ -11,26 +11,24 @@ const LoadingFull = () => {
   const alertStore = useAlert()
 
   return (
-    <>
-      <Dialog
-        open={alertStore.is_Loading}
-        disableEscapeKeyDown
-        aria-labelledby='alert-dialog-title'
-        aria-describedby='alert-dialog-description'
-        sx={{maxWidth: '50%', justifySelf: 'center'}}
-        onClose={(event, reason) => {
-          if (reason !== 'backdropClick') {
-            handleClose()
-          }
-        }}
-      >
-        <DialogContent sx={{ padding: 10 }}>
-          <DialogContentText id='alert-dialog-description'>
-            <CircularProgress thickness={5} />
-          </DialogContentText>
-        </DialogContent>
-      </Dialog>
-    </>
+    <Dialog
+      open={alertStore.is_Loading}
+      disableEscapeKeyDown
+      aria-labelledby='alert-dialog-title'
+      aria-describedby='alert-dialog-description'
+      sx={{ maxWidth: '50%', justifySelf: 'center' }}
+      onClose={(event, reason) => {
+        if (reason !== 'backdropClick') {
+          handleClose()
+        }
+      }}
+    >
+      <DialogContent sx={{ padding: 10 }}>
+        <DialogContentText id='alert-dialog-description'>
+          <CircularProgress thickness={5} />
+        </DialogContentText>
+      </DialogContent>
+    </Dialog>
   )
 }
 
